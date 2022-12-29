4d352e3f682f961ab0f68894a66989856ae884cc
tk <trishank01@gmail.com>
2022-12-30 01:30:42 +0530

    added transaction form connected to express and mongodb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import  { useEffect, useState } from "react"
 import './App.css';
 
 function App() {
+  const [form, setForm] = useState({
+    amount : 0,
+    description: "",
+    date : ""
+  })
+  const [formSubmit, setFormSubmit] = useState([])
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setFormSubmit([...formSubmit , form])
+     await fetch("http://localhost:4000/transaction" , {
+      method : "POST",
+      body : JSON.stringify(form),
+      headers : {
+        'content-type' : "application/json",
+      }
+    })
+
+
+    setForm({
+      amount : 0,
+      description: "",
+      date : ""
+    })
+  }
+
+  const handleInput = (e) => {
+       setForm({...form , [e.target.name]:e.target.value})
+
+  }
+ 
+  useEffect(() => {
+    fetchTransactions()
+  })
+
+  const fetchTransactions = async () => {
+    const res = await fetch("http://localhost:4000/transaction")
+    const {data} = await res.json()
+    console.log(data)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+        <input type="number" name="amount" value={form.amount} placeholder="Enter transaction amount" onChange={handleInput}/>
+        <input type="text" name="description" value={form.description} placeholder="Enter transaction details" onChange={handleInput}/>
+        <input type="date"  name="date" value={form.date} onChange={handleInput}/>
+        <button type="submit">Submit</button>
+    </form>
+    <br/>
+    <table>
+      <thead>
+        <th>Amount</th>
+        <th>Description</th>
+        <th>Date</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>sddsf</td>
+          <td>23432</td>
+        </tr>
+      </tbody>
+    </table>
+    </>
   );
 }
 
